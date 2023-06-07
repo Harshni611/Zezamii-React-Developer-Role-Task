53c5ee3425df102c2a3bbea105cdd174538686cb
Harshni Ravichandran <86094768+Harshni611@users.noreply.github.com>
2023-06-07 14:47:19 +1000

    Add files via upload

---
+++ UsersList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Card from '../UI/Card';
+import classes from './UsersList.module.css';
+
+const UsersList = ({users}) => {
+	return (
+		<Card className={classes.users}>
+	<ul>
+				{users.length > 0 ?
+					users.map(user=>(
+						<li key={user.id}>{user.username} ({user.age} years old)</li>
+					))
+					:
+					<p>There are no users yet</p>
+				}
+			</ul>
+		</Card>
+	);
+};
+
+export default UsersList;
